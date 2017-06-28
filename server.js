5fc680cc13ca7ef77e1fd72b0a42540ef99798c4
saumyadixit <sxd160630@utdallas.edu>
2017-06-27 20:29:17 -0500

    Added HTTPS support, will now work on mobile browsers

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var expressLayouts = require('express-ejs-layouts');
 var BinaryServer = require('binaryjs').BinaryServer;
 var fs = require('fs');
 var wav = require('wav');
+
+var key = fs.readFileSync('./cert/key-20170627-165927.pem');
+var cert = fs.readFileSync('./cert/cert-20170627-165927.crt' );
+var ca = fs.readFileSync( './cert/cert-key-20170627-165927.p12' );
+
 //var sox = require('sox');
 var outFile = 'demo.wav';
 
@@ -18,12 +23,42 @@ app.use(expressLayouts);
 //Set static files (css and image)public files locations
 app.use(express.static(__dirname+'/public'));
 //start the server
+var options = {
+  key: key,
+  cert: cert,
+  ca: ca
+};
 
-app.listen(port, '0.0.0.0', function() {
+var https = require('https');
+https.createServer(options, app).listen(port, '0.0.0.0', function() {
     console.log('Listening to port:  ' + port);
 });
+//
+//app.listen(port, '0.0.0.0', function() {
+//    console.log('Listening to port:  ' + port);
+//});
+/*
+var speech = require('@google-cloud/speech')({
+    projectId: 'my-project-1479251894350',
+    keyFilename: './VoiceRecog-6083eb45cc69.json'
+});
 
+const request = {
+    encoding: 'FLAC',
+    sampleRateHertz: 16000,
+    languageCode: 'en-US'
+};
 
+// Detects speech in the audio file
+speech.recognize('temp.flac', request)
+      .then((results) => {
+        const transcription = results[0];
+        console.log(`Transcription: ${transcription}`);
+      })
+      .catch((err) => {
+        console.error('ERROR:', err);
+});
+*/
 binaryServer = BinaryServer({port: 9001});
 
 binaryServer.on('connection', function(client) {
