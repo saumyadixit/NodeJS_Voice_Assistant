d2dd0efb96514f3e102a230fa88007965a93f212
saumyadixit <sxd160630@utdallas.edu>
2017-07-21 13:23:15 -0400

    Added comments

--- app/routes.js
+++ app/routes.js
@@ -1,59 +1,68 @@
-//Requires express
+//Requires express for express layout
 var express = require('express');
 var path = require('path');
 var url = require( "url" );
 var queryString = require( "querystring" );
+
+//Initialize linear16 plugin for conversion of audio to linear16 format from WAV
 const linear16 = require('linear16');
 //Create router object
 var router = express.Router();
 //Export our router
 module.exports = router;
-//Route for Home Page
+//Define Route for Home Page
 router.get('/', function(req, res){
+  //On get request render index.ejs page
   res.render('pages/index')
 });
 
-
+//Initialize HTTP for interacting with Spring Boot NLP server over HTTP
 var http = require('http');
 
+//Transcription from Google Speech API
 var transcription;
+//Output Intent
 var output_intent;
+//Initialize file names for original recorder WAV file and the converted LINEAR16 file
 var input_file="File0"+".wav";
 var output_file="File0"+"_conv.wav";
 
-//Test run for ajax callback
+//Handle Post Request from Front End to /speech URL. This request
 router.post('/speech', function(req, res){
-
+        //Extract jsonData from request body
         var txt = req.body.jsonData;
-        //console.log( txt );
-        //var count =1;
         input_file = "File"+(count-1)+".wav";
         output_file = "File"+(count-1)+"_conv.wav";
 
         console.log('Calling conversion utility');
         //Conversion to linear16
 
-
+        //Create new promise for File Conversion
         var conversion_complete = new Promise(
             function (resolve, reject){
               try {
+                  //Set File
                   var file =
                   {
                     inp_file: input_file
                   };
+                  //Debug lof the input and output file
                   console.log(input_file);
                   console.log(output_file);
+                  //Set the parms as the input and output file
                   const params = {
                       input: input_file,
                       output: output_file
                   };
-
+                  //Resolve the parms
                   Promise.resolve(params)
+                      //On File exists
                       .then(paths => {
+                          //Convert and return file
                           return linear16(input_file, output_file);
                       })
                       .then(wavFile => {
-                          //Conversion done
+                          //Conversion is complete now, resolve the conversion_complete promise
                           resolve(file);
                           console.log("File Conversion Completed .. ");
                       })
@@ -65,14 +74,19 @@ router.post('/speech', function(req, res){
         });
 
         //Conversion to linear16 complete
-
-
+        //In NodeJS the javascript functions are asynchronous, so promises are way to make them work synchronously
+        //Use all the promises created
+        //Call conversion_complete function
         conversion_complete
+          //on resolved call promise function recognize_google
           .then(recognize_google)
+          //on resolved call promise function get_intent
           .then(get_intent)
+          //on completion of get_intent send the output_intent JSON to the front end
           .then(function(fullfilled)
           {
             console.log("All Process Complete" + fullfilled);
+            //Send data to Result object to front end
             res.send(output_intent);
           })
           .catch(function (error) {
@@ -82,38 +96,37 @@ router.post('/speech', function(req, res){
         });
 });
 
-
-
-
-
-
-
-//Speech recognition starts
-
+//Define Speech recognition function promise
 var recognize_google = function(file){
   return new Promise(
       function(resolve,reject){
           console.log("Google Recognition Started ... "+output_file);
+          //Initialize the project name for Google Cloud in projectId
+          //Set location of authentication key JSON in keyFilename
           var speech = require('@google-cloud/speech')({
               projectId: 'my-project-1479251894350',
               keyFilename: './VoiceRecog-6083eb45cc69.json'
           });
 
+          //Initialize properties of request object
           const request = {
               encoding: 'LINEAR16',
               sampleRateHertz: 16000,
               languageCode: 'en-US',
               speechContexts: {
-                  'phrases':['Saumya','Dixit','Joydeep','Roy']
+                  'phrases':['Saumya','Dixit'] // Pass context phrases for more accurate conversion
               }
           };
 
-          // Detects speech in the audio file
+          // Detects speech in the audio file. Call the recognise function for the last output file and pass the request object
           speech.recognize(output_file, request)
                 .then((results) => {
+                  //Get the first Transcription
                   transcription = results[0];
+                  //We can process all the possible transcriptions as part of enhancement
                   console.log("Google Speech Transcription Completed .. ");
                   console.log(`Transcription: ${transcription}`);
+                  //Resolve recognize_google promise
                   resolve(transcription);
                 })
                 .catch((err) => {
@@ -125,7 +138,7 @@ var recognize_google = function(file){
 };
 
 
-//Make get call to Spring Boot
+//Initialize function get_intent. This function is called on receiving AJAX request for intent analysis from front end (If keyword detected)
 
 var get_intent = function(transcription){
   return new Promise(
@@ -134,28 +147,29 @@ var get_intent = function(transcription){
           console.log("Intent Processing started ... "+transcription);
           transcription=encodeURIComponent(transcription.trim());
           var optionsget = {
-              host : 'localhost', // here only the domain name
-              // (no http/https !)
-              port : 4050,
-              path : '/process?text='+transcription, // the rest of the url with parameters if needed
-              method : 'POST' // do GET
+              host : 'localhost', // The domain name of Spring Boot Server (Localhost as its running currently on the same server as NodeJS)
+              port : 4050, // Set port of Spring Boot Server
+              path : '/process?text='+transcription, // Pass the transcription from Google Speech API
+              method : 'POST' // Do a POST request
           };
 
+          //Logging some debug info
           console.info('Options prepared:');
           console.info(optionsget);
           console.info('Do the GET call');
 
-          // do the GET request
+          //Do the GET request over HTTP with options as defined in optionsget
           var reqGet = http.request(optionsget, function(res) {
               console.log("statusCode: ", res.statusCode);
-
+              //On receiving result from Spring Boot Server
               res.on('data', function(chunk) {
-                  console.log('Spring Boot Call Processed .. ')
+                  console.log('Spring Boot Call Processed .. ');
+                  //Log the output JSON from Spring Boot Server
                   console.log('Output : ' + chunk);
                   output_intent=chunk;
+                  //Mark promise as resolved for Intent Processing using Spring Boot NLP server
                   resolve(chunk);
-                  //process.stdout.write(d);
-                  //console.info('\n\nCall completed');
+
               });
 
           });
@@ -164,7 +178,7 @@ var get_intent = function(transcription){
           reqGet.on('error', function(e) {
               console.error(e);
           });
-          //return output_intent;
+
         }
 );
 };
--- server.js
+++ server.js
@@ -1,42 +1,47 @@
-//Dependencies
+//Adding Express support to Node Site
 var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
-
+//Initialize BinaryServer for binary audio streaming from front end
 var BinaryServer = require('binaryjs').BinaryServer;
+//Initialize FS for handling writing of audio to file
 var fs = require('fs');
+//Initialize WAV plugin for handling wav encoding
 var wav = require('wav');
 
 
+
+
+var outFile = 'InpFile';
+
+//Create Instance of express
+var app = express();
+//Initialize body-parser for parsing JSON data body in AJAX request from front end
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+var port = 8080; // App will listen to https requests on port 8080
+var router = require('./app/routes'); //Include routes.js file
+app.use('/', router); // Set Home directory
+
+app.set('view engine','ejs');
+app.use(expressLayouts);
+//Set static files public files locations
+app.use(express.static(__dirname+'/public'));
+
+//Initialize Keys and Certificates for authentication of HTTPS protocol
+//Reference to generate SSH key : https://docs.nodejitsu.com/articles/HTTP/servers/how-to-create-a-HTTPS-server/
 var key = fs.readFileSync('./cert/key-20170627-165927.pem');
 var cert = fs.readFileSync('./cert/cert-20170627-165927.crt' );
 var ca = fs.readFileSync( './cert/cert-key-20170627-165927.p12' );
 
-//var sox = require('sox');
-var outFile = 'InpFile';
-
-var app = express();
-var bodyParser = require('body-parser');
-// parse an HTML body into a string
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-var port = 8080;
-var router = require('./app/routes');
-app.use('/', router);
-
-app.set('view engine','ejs');
-app.use(expressLayouts);
-//Set static files (css and image)public files locations
-app.use(express.static(__dirname+'/public'));
-
-
-//start the server
+//Define authentication options for the HTTPS server
 var options = {
   key: key,
   cert: cert,
   ca: ca
 };
 
-
+//Initialize and create HTTPS server
 var https = require('https');
 httpsServer = https.createServer(options, app);
 httpsServer.listen(port, '0.0.0.0', function() {
@@ -44,35 +49,41 @@ httpsServer.listen(port, '0.0.0.0', function() {
 });
 
 
+//Initialize HTTP
 var http = require('http');
 
-
-//BinaryServer code starts
-
-
+//BinaryServer Initialization on HTTPS server as base
 binaryServer = BinaryServer(
   {
     server: httpsServer
   });
+//Initialize File Count to 0 when server is started
 count = 0;
+//On New Binary Connection
 binaryServer.on('connection', function(client) {
   console.log('new connection');
+  //Increase File Count on each new request
   var outFile = "File"+count+".wav";
   count++;
+  //Initialize new WAV File Writer with below properties
   var fileWriter = new wav.FileWriter(outFile, {
     channels: 1,
     sampleRate: 48000,
     bitDepth: 16
   });
+  //On Stream of Binary Audio Data from Front End to NodeJS
   client.on('stream', function(stream, meta) {
       console.log('new stream');
+      //Pipe the input data to File writer, so that it keeps writing the audio from microphone to the wav file
       stream.pipe(fileWriter);
-
+      //On end of Stream from Front End to NodeJS
       stream.on('end', function() {
+          //Close File Writer Object
           fileWriter.end();
           console.log('wrote to file ' + outFile);
 
     });
+        //On Error Log the Error
       stream.on('error',function(err){
         console.log(err);
       });
